a4ed9b62b81865faf7c02865d22819cadc5242d2
swetajha99 <jhasweta813@gmail.com>
2022-08-27 22:35:41 +0530

    first-commit

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { FaSearch, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
 function Header() {
@@ -18,7 +17,7 @@ function Header() {
 
         <div className='header__search'>
             <input className='header__searchInput' type="text"></input>
-            <SearchIcon className='header__searchIcon'/>
+            <FaSearch className='header__searchIcon'/>
         </div>
         <div className='header__nav'>
         <Link to='/login'>
@@ -40,7 +39,7 @@ function Header() {
 
                 <Link to="/checkout">
                 <div className="header__optionBasket">
-                <ShoppingCartIcon />
+                <FaShoppingCart />
                  <span className='header__optionLineTwo header__basketCount'>{basket?.length}</span> {/* <!--optional chaining--> */}
                 </div>
                </Link>
