167049c4a7b8b9eef34b8156bc9dd565086c2f17
Sweta Jha <98458809+swetajha99@users.noreply.github.com>
2022-09-27 18:33:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Checkout from './Checkout';
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
     <Header/>
-
-    <Routes>
     
       <Route path='/checkout' element={<Checkout />} />
 
 {/* Default route should be at the bottom */}
       <Route path='/'  element={<Home/>} /> 
-    
-      </Routes>
+
     </div>
     </BrowserRouter>
   );
